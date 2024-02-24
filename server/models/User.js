bd2cb5d89cfdae665b81d42732e4747d7be95a10
surajjayraman <suraj.jayraman@gmail.com>
2024-02-24 15:20:28 -0500

    Update User schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,25 +1,47 @@
 // User schema
 const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImagePath: {
+      type: String,
+      default: "",
+    },
+    tripList: {
+      type: Array,
+      default: [],
+    },
+    wishList: {
+      type: Array,
+      default: [],
+    },
+    propertyList: {
+      type: Array,
+      default: [],
+    },
+    reservationList: {
+      type: Array,
+      default: [],
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  profileImagePath: {
-    type: String,
-    default: "",
-  },
-});
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
