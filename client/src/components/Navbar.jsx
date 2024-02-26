04704d6a4a66772ddb4687544c0ca91a32e3d2ef
surajjayraman <suraj.jayraman@gmail.com>
2024-02-25 20:09:53 -0500

    Update Navbar for conditional rendering

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import { IconButton } from "@mui/material";
 import { Search, Person, Menu } from "@mui/icons-material";
 import variables from "../styles/variables.scss";
+import "../styles/Navbar.scss";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { setLogout } from "../redux/state";
 
 const Navbar = () => {
   const [dropdownMenu, setDropdownMenu] = useState(false);
   const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
   return (
     <div className="navbar">
@@ -20,22 +26,51 @@ const Navbar = () => {
       </div>
       <div className="navbar_right">
         {user ? (
-          <a href="/create-listing">Become A Host</a>
+          <a href="/create-listing" className="host">
+            Become A Host
+          </a>
         ) : (
-          <a href="/login">Become A Host</a>
+          <a href="/login" className="host">
+            Become A Host
+          </a>
         )}
-        <button className="navbar_right_account">
+        <button
+          className="navbar_right_account"
+          onClick={() => setDropdownMenu(!dropdownMenu)}
+        >
           <Menu sx={{ color: variables.darkgrey }} />
           {!user ? (
             <Person sx={{ color: variables.darkgrey }} />
           ) : (
             <img
-              src={`/${user.profileImagePath.replace("public", "")}`}
+              src={`http://localhost:3001/${user.profileImagePath.replace(
+                "public",
+                ""
+              )}`}
               alt="profile pic"
               style={{ objectFit: "cover", borderRadius: "50%" }}
             />
           )}
         </button>
+        {dropdownMenu && !user && (
+          <div className="navbar_right_accountmenu">
+            <Link to="/login">Log In</Link>
+            <Link to="/register">Sign Up</Link>
+          </div>
+        )}
+
+        {dropdownMenu && user && (
+          <div className="navbar_right_accountmenu">
+            <Link to="">Trip List</Link>
+            <Link to="">Wish List</Link>
+            <Link to="">Property List</Link>
+            <Link to="">Reservation List</Link>
+            <Link to="">Become A Host</Link>
+            <Link to="/login" onClick={() => dispatch(setLogout())}>
+              Log Out
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
