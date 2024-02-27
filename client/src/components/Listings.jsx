0ffcdf0c00af8d06d819c7275400cd980576e5dc
surajjayraman <suraj.jayraman@gmail.com>
2024-02-27 11:30:46 -0500

    Update home page with listings

--- client/src/components/Listings.jsx
+++ client/src/components/Listings.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
+import { categories } from "../data";
+import "../styles/Listings.scss";
 
 const Listings = () => {
   return (
-    <div>
-      {/* Your component code here */}
+    <div className="category-list">
+      {categories?.map((category, index) => (
+        <div key={index} className="category">
+          <div className="category_icon">{category.icon}</div>
+          <p>{category.label}</p>
+        </div>
+      ))}
     </div>
   );
 };
 
 export default Listings;
-
-
